48af46728e92de952c4859d5c617389292bd309a
Darron Haworth <darron.haworth@firsttechfed.com>
2019-10-28 14:39:56 -0700

    init

--- DeleteQuartzApp/Program.cs
+++ DeleteQuartzApp/Program.cs
@@ -1,15 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Quartz;
+using System;
+using Topshelf;
+using Topshelf.Quartz;
 
-namespace DeleteQuartzApp
+namespace mpustelak.TopShelfAndQuartz
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Topshelf.HostFactory.Run(x =>
+            {
+                x.Service<MyService>(s =>
+                {
+                    s.WhenStarted(service => service.OnStart());
+                    s.WhenStopped(service => service.OnStop());
+                    s.ConstructUsing(() => new MyService());
+
+
+                    s.ScheduleQuartzJob(q =>
+                        q.WithJob(() =>
+                            JobBuilder.Create<SecondsJob>().Build())
+                            .AddTrigger(() => TriggerBuilder.Create()
+                                .WithSimpleSchedule(b => b
+                                    .WithIntervalInSeconds(15)
+                                    .RepeatForever())
+                                .Build()));
+
+                    s.ScheduleQuartzJob(q =>
+                        q.WithJob(() =>
+                            JobBuilder.Create<MinuteJob>().Build())
+                            .AddTrigger(() => TriggerBuilder.Create()
+                                .WithSimpleSchedule(b => b
+                                    .WithIntervalInMinutes(1)
+                                    .RepeatForever())
+                                .Build()));
+                });
+
+                x.RunAsLocalSystem()
+                    .DependsOnEventLog()
+                    .StartAutomatically()
+                    .EnableServiceRecovery(rc => rc.RestartService(1));
+
+                x.SetServiceName("My Topshelf Service");
+                x.SetDisplayName("My Topshelf Service");
+                x.SetDescription("My Topshelf Service's description");
+            });
+        }
+    }
+
+    public class MyService
+    {
+        public void OnStart()
+        {
+        }
+
+        public void OnStop()
+        {
+        }
+    }
+
+    public class MinuteJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var dt = DateTime.Now;
+            Console.WriteLine($"[{DateTime.Now}] Minute job");
+        }
+    }
+
+    public class SecondsJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            
+            Console.WriteLine($"[{DateTime.Now}] 15 seconds job!");
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# WorkingQuartzSample
+
+## Darron H
